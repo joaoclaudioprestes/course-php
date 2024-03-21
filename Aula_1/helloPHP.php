3b55e7b542a6453b9026714b43d415e36a754824
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-03-20 23:22:05 -0300

    Update helloPHP.php

--- Aula_1/helloPHP.php
+++ Aula_1/helloPHP.php
@@ -1,3 +1,26 @@
 <?php
-echo "Hello PHP"
-?>
+echo "Hello PHP <br>";
+
+// Comentários
+
+// Comandos de saída
+echo "Curso de PHP <br>";
+echo "jprestes <br>";
+
+print("Aula de PHP <br>");
+print("Novo curso!! <br>");
+
+// Váriaveis
+$_first_name = "João";
+$_age = 19;
+
+// Mostra informações sobre as variaveis
+var_dump($_first_name);
+
+echo "<p>Olá eu sou o $_first_name</p>";
+
+// Criar constantes
+define("TAXA", 0.5);
+
+// Escrever o valor de constantes
+echo "TAXA " . TAXA . "%<br>";
